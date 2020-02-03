5681e2035657acaca377ebeeb0f0196186930034
silverscat-3 <silverscat_3@mail.sc3.fun>
2020-02-03 14:39:14 +0900

    :pencil2:Content-Typeを吐くように

--- handlers/hostmeta.go
+++ handlers/hostmeta.go
@@ -34,5 +34,6 @@ func (h *HostMetaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+	w.Header().Add("content-type", "application/xrd+xml")
 	w.Write(body)
 }
